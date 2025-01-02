f092db0d8523efd5be236da227f5aeb58650851f
DmitryMVP <masqueunclub17@gmail.com>
2025-01-02 19:42:27 +0500

    optimaze script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '50');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +11,33 @@ const personalMovieDB = {
 };
 
 
+for (let i = 0; i < 2; i++) {
 
-const firstWatchedFilm = prompt('Один из последних просмотренных фильмов?', '');
-const  firstFilmRating = +prompt('На сколько оцените его?', '');
-const secondWatchedFilm = prompt('Один из последних просмотренных фильмов?', '');
-const  secondFilmRating = +prompt('На сколько оцените его?', '');
+    let a = prompt('Один из последних просмотренных фильмов?', 'q');
+    let b = +prompt('На сколько оцените его?', '2');
 
-personalMovieDB.movies[firstWatchedFilm] = firstFilmRating;
-personalMovieDB.movies[secondWatchedFilm] = secondFilmRating;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b != NaN) {
+        personalMovieDB.movies[a] = b;
+            console.log('good');
+    } else {
+        i--;
+        console.log('error')
+    }
+}
 
+switch (true) {
+    case personalMovieDB.count < 10:
+        console.log('Просмотренно довольно мало фильмов');
+        break;
+    case personalMovieDB.count >= 10 && personalMovieDB.count <= 30:
+        console.log('Вы классический зритель');
+        break;
+    case personalMovieDB.count > 30:
+        console.log('Вы киноман');
+        break;
+    default:
+        console.log('Произошла ошибка');
+        break;
+}
+ 
 console.log(personalMovieDB);
