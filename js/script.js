9f34cc0f3723a48fe8c5a3a20c2914ad713db0c5
DmitryMVP <masqueunclub17@gmail.com>
2025-01-12 09:55:54 +0500

    version 1.0

--- js/script.js
+++ js/script.js
@@ -3,23 +3,22 @@
 
 
 const personalMovieDB = {
-    count: '',
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: true,
     start: () => {
-  personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '50');
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '50');
 
-        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '50');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '50');
         }
-
-        personalMovieDB.count = numberOfFilms;
     },
+
     rememberMyFilms: () => {
         for (let i = 0; i < 2; i++) {
-            let a = prompt('Один из последних просмотренных фильмов?', 'q');
+            let a = prompt('Один из последних просмотренных фильмов?', 'q').trim();
             let b = +prompt('На сколько оцените его?', '2');
 
             if (a != null && b != null && a != '' && b != '' && a.length < 50 && b != NaN) {
@@ -57,16 +56,27 @@ const personalMovieDB = {
     },
     writeYourGenres: () => {
 
-        for (let i = 0; i < 3; i++) {
-            let genres = prompt(`Ваш любимый жанр под номером ${i + 1}`)
+        for (let i = 0; i < 1; i++) {
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+            // if (genres !== null && genres !== '') {
+            //     personalMovieDB.genres[i] = genres;
+            // } else {
+            //     i--;
+            // }
 
             if (genres !== null && genres !== '') {
-                personalMovieDB.genres[i] = genres;
+                personalMovieDB.genres = genres.split(', ')
+                personalMovieDB.genres.sort();
             } else {
                 i--;
             }
 
         }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`)
+        });
     },
 };
 
